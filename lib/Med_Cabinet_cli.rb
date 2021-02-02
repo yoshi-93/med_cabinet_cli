2d3504a17888f8a4bdf9050e8bc3e4a0ec580df6
Mario Lopez <ez0212sp@icloud.com>
2021-02-01 19:36:58 -0500

    commit tree 05ee2b1b3fa54d5036d9e37557ed3e08e688a775

--- lib/Med_Cabinet_cli.rb
+++ lib/Med_Cabinet_cli.rb
@@ -1,10 +1,20 @@
 require "pry"
+
+
 require "Med_Cabinet_cli/version"
+
+
+require_relative "./Med_Cabinet_cli/Med_Cabinet"
+require_relative "./Med_Cabinet_cli/api"
+
+
+
+
 
 #require all files here
 #require all "TOOLSETS/LIBRARIES"
 
-module MedCabinetCli
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module MedCabinetCli
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
---
+++ lib/Med_Cabinet_cli/med_cabinet.rb
@@ -0,0 +1,4 @@
+def testing
+    puts "Hello"
+  
+end
